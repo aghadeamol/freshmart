ddb061e7fc05baef1e5ef394228acde99c28361b
aghadeamol <amol.aghade@techbirdit.in>
2026-03-18 13:10:09 +0530

    Update for Render production deployment (env vars & ports)

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cartRouter = require("./routes/cart");
 const ordersRouter = require("./routes/orders");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
